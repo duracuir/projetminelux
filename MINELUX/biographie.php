7e288dcd64251a7f9b274efd20a9bfbb3dca75b8
Serges YAMAKO <yamakoserges@gmail.com>
2020-08-31 15:00:04 +0100

    pieds de page et certains pages

--- MINELUX/biographie.php
+++ MINELUX/biographie.php
@@ -1,32 +1,10 @@
-<?php
-session_start();
-  // require('connexionBD.php');
-$connection = mysqli_connect("localhost", "root", "");
-$db = mysqli_select_db($connection, 'myminette');
-if(isset($_POST['Save'])) {
-    $name = $_GET["username"];
- 
- $query = "UPDATE `membres` SET datenaiss = '$_POST[datenais]', slogan = '$_POST[slogan]', region = '$_POST[region]', nationality = '$_POST[nationality]', epilation = '$_POST[epilation]', piercing = '$_POST[piercing]', tattoo = '$_POST[tattoo]' WHERE username = '$_GET[username]' ";
-  $query_run = mysqli_query($connection, $query);
-  if($query_run) {
-    echo '<script type="text/javascript"> alert("Vos données ont été enregistrées avec succès") </script>';
-  } else {
-    echo '<script type="text/javascript"> alert("Echecs enregistrement de vos donnees") </script>';
-  }
-}
 
-if(!isset($_SESSION['username'])) {
-    header('location:connexion.php');
-    exit;
-}
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
-    <tilte></tilte>
+	<tilte></tilte>
         <meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -35,8 +13,39 @@ if(!isset($_SESSION['username'])) {
         <script src="js/script.js"></script>
 </head>
     <body id="edit-profile">
-         <header>
-            
+        <svg id="svg-source" height="0" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+        <filter id="filter-shadow" x="0" y="0" width="100%" height="100%">
+            <feGaussianBlur in="SourceAlpha" stdDeviation="3"></feGaussianBlur>
+            <feOffset dx="1" dy="1"></feOffset>
+            <feComponentTransfer>
+                <feFuncA type="linear" slope="0.3"></feFuncA>
+            </feComponentTransfer>
+            <feMerge>
+                <feMergeNode></feMergeNode>
+                <feMergeNode in="SourceGraphic"></feMergeNode>
+            </feMerge>
+        </filter>
+    </defs>
+    
+    
+    
+    
+    <g id="icon-real-photos">
+        <polygon points="16.67,4 13.85,20 16.89,20 19.1,7.48 56.52,14.07 52.18,38.69 48,37.96 48,50.14 53.05,51.03 60,11.64"></polygon>
+        <path d="M4,20v40h44V20H4z M45,48H7V23h38V48z M41,45H11l5-7l5,4l5-7l5,5l5-8L41,45z M15,28c-1.66,0-3,1.34-3,3c0,1.66,1.34,3,3,3 c1.66,0,3-1.34,3-3C18,29.34,16.66,28,15,28z"></path>
+    </g>
+    <g id="icon-discretion-guaranteed">
+        <path d="M50,28v-6c0-9.93-8.08-18-18-18c-9.93,0-18,8.07-18,18v6H8v32h48V28H50z M34,42.45V51c0,1.1-0.9,2-2,2s-2-0.9-2-2v-8.55 c-1.19-0.69-2-1.97-2-3.45c0-2.21,1.79-4,4-4s4,1.79,4,4C36,40.48,35.19,41.75,34,42.45z M18,28v-6c0-7.72,6.28-14,14-14 s14,6.28,14,14v6H18z"></path>
+    </g>
+    <g id="icon-trusted-website">
+        <path d="M49.97,19.19L49.32,14l-4.17-3.15L42,6.68l-5.19-0.65L32,4l-4.81,2.03L22,6.68l-3.15,4.17L14.68,14l-0.65,5.19L12,24 l2.03,4.81L14.68,34l4.17,3.16L22,41.32l5.19,0.65L32,44l4.81-2.03L42,41.32l3.15-4.17L49.32,34l0.65-5.19L52,24L49.97,19.19z M34.5,32.54h-4v-11.5l-3,1.5v-4l4-2h3V32.54z"></path>
+        <path d="M56,54l-7.7-1.33L45.61,60l-8.58-14.86l0.57-0.24l4.77-0.6l1.25-0.16l0.76-1.01l2.9-3.83l0.15-0.11L56,54z M26.4,44.89 l-4.77-0.6l-1.26-0.16l-0.76-1.01l-2.9-3.83l-0.15-0.11L8,54l7.7-1.33l2.7,7.33l8.58-14.86L26.4,44.89z"></path>
+    </g>
+    
+</svg>
+        <header>
+        	
                 <div class="top-nav">
                 <div class="container clearfix">
                     <ul class="nav navbar-nav">
@@ -47,23 +56,9 @@ if(!isset($_SESSION['username'])) {
 
                     <div class="pull-left"></div>
                     <div class="pull-right">
-                         <div><p>Bienvenu:
-                        <a class="btn btn-primary" href="login.html">
-                             <?php 
-                       
-                                if(!isset($_SESSION['username'])){
-                                   echo "Vous n'êtes pas connecté!";
-                                }else{
-                                    echo $_SESSION['username'];
-                                
-                                }
-                     ?>
-                        </a>
-
-                                </p>
-                            </div>
-                        <a class="btn btn-secondary" href="contact.html">Nous Contacter</a>
-                        <a class="btn btn-secondary" href="deconnexion.php">Deconnexion</a>
+                        <a class="btn btn-primary" href="login.html">S'identifier</a>
+                        <a class="btn btn-tertiary" href="signup.html">S'inscrire</a>
+                        <a class="btn btn-secondary" href="contact.html">Nous Contater</a>
                     </div>
             
                 </div>
@@ -99,37 +94,37 @@ if(!isset($_SESSION['username'])) {
             </div>
             </div>
             </div>
-        </header>
+		</header>
            <main class="container">
-        <form id="bio_submit" class="submit_profile" action="biographie.php?username=<?php echo $_GET['username'];?>" method="post">
+        <form action id="bio_submit" class="submit_profile" method="post">
             <ul class="steps">
-                            <li class="active"><a href="#">Step 1:<br> Biographie</a></li>
-                            <li class=""><a href="aproposdemoi.php">Step 2:<br>A propos de moi</a></li>
-                            <li class=""><a href="langue.php">Step 3:<br>Langues</a></li>
-                            <li class=""><a href="#">Step 4:<br>Villes de travail</a></li>
-                            <li class=""><a href="services.php">Step 5:<br>Service</a></li>
-                            <li class=""><a href="mon-cadeau.php">Step 6:<br>Mon Cadeau</a></li>
-                            <li class=""><a href="#">Step 7:<br>Coordonnées</a></li>
+                            <li class="active"><a href="/panel/profile/biography/9024/">Step 1:<br> Biographie</a></li>
+                            <li class=""><a href="/panel/profile/about_me/9024/">Step 2:<br>A propos de moi</a></li>
+                            <li class=""><a href="/panel/profile/languages/9024/">Step 3:<br>Langues</a></li>
+                            <li class=""><a href="/panel/profile/working_cities/9024/">Step 4:<br>Villes de travail</a></li>
+                            <li class=""><a href="/panel/profile/services/9024/">Step 5:<br>Service</a></li>
+                            <li class=""><a href="/panel/profile/rates/9024/">Step 6:<br>Mon Cadeau</a></li>
+                            <li class=""><a href="/panel/profile/contact/9024/">Step 7:<br>Coordonnées</a></li>
             </ul>
         
-        <div class="row rows-17">
+        <div class="row row-17">
             <div class="col-xs-6">
-               <div class="row rows-17">
-                   <div class="col-xs-12">
-                       <div class="box">
-                           <h3 class="heading">Présentation</h3>
-                           <div class="row">
+               <div class="row row-17">
+				   <div class="col-xs-12">
+				   <div class="box">
+					   <h3 class="heading"> presentation </h3>
+					   <div class="row">
                                <div class="col-xs-3 grp">
                             <label for="nom">Nom*:</label>
                         </div>
                             <div class="col-xs-5 grp">
-                            <input type="text" id="nom" name="username" value="<?php echo $_SESSION['username'] ?>" placeholder="Nom">
+                            <input type="text" id="nom" name="nom" value="" placeholder="Nom">
                             </div>
                             <div class="col-xs-5 grp">
                             <label for="start">Né(e) le:</label>
                         </div>
                             <div class="col-xs-5 grp">
-                            <input class="zone" id="date" name="datenais" type="date" value=""  placeholder="date de naissance"> 
+                            <input class="zone" id="date" id="start" name="trip-start" type="date" value="" min="1999-01-01" max="2080-01-01" placeholder="date de naissance"> 
                             </div>
                             <div class="col-xs-3 grp">
                             <label for="slogan">Slogan:</label>
@@ -143,12 +138,12 @@ if(!isset($_SESSION['username'])) {
                         <div class="col-xs-5 grp">
                             <div class="bloc">
                                   <div class="select">
-                                    <select name="region">
+                                    <select>
                                       <option>De quelle Region est vous? </option>
-                                      <option value="Centre">Centre</option>
-                                      <option value="Nord">Nord</option>
-                                      <option value="Ouest">Ouest</option>
-                                      <option value="Sud-Ouest">Sud-Ouest</option>
+                                      <option value="0">Centre</option>
+                                      <option value="1">Nord</option>
+                                      <option value="2">Ouest</option>
+                                      <option value="3">Sud-Ouest</option>
                                       <option value="4">Autre a préciser</option>
                                     </select>
                                   </div>
@@ -162,12 +157,12 @@ if(!isset($_SESSION['username'])) {
                         <div class="col-xs-5 grp">
                             <div class="bloc">
                                   <div class="select">
-                                    <select name="nationality">
-                                      <option value="Camerounais(e)">Camerounais(e)</option>
-                                      <option value="Gabonais(e)">Gabonais(e)</option>
-                                      <option value="Tchadien(ne)">Tchadien(ne)</option>
-                                      <option value="Nigerien(ne)">Nigerien(ne)</option>
-                                      <option value="Congolais(e)">Congolais(e)</option>
+                                    <select>
+                                      <option value="active">Camerounais(e)</option>
+                                      <option value="0">Gabonais(e)</option>
+                                      <option value="1">Tchadien(ne)</option>
+                                      <option value="2">Nigerien(ne)</option>
+                                      <option value="3">Congolais(e)</option>
                                       <option value="4">Autre a préciser</option>
                                     </select>
                                   </div>
@@ -175,62 +170,72 @@ if(!isset($_SESSION['username'])) {
                             </div>
                          </div>
                        </div>
-                   </div>
-               </div>
-               <div class="col-xs-6">
-            <div class="box">
-                <h3 class="heading">Option</h3>
-                     <div class="rows">
+					   
+				    </div>
+				  </div>
+			 
+		    
+			<div class="col-xs-6">
+				   <div class="box">
+					   <h3 class="heading"> option </h3>
+					   <div class="row">
                         <div class="col-xs-3 grp">
-                        <label for="ethnicity">Epilation du Maillot:*</label>
+                        <label for="ethnicity">Epilation Maillot:*</label>
                         </div>
                         <div class="col-xs-5 grp">
                             <div class="bloc">
                                   <div class="select">
-                                    <select name="epilation">
-                                      <option value="Entierement Naturel">Entierement Naturel</option>
-                                      <option value="Entierement rasés">Entierement rasés</option>
-                                      <option value="Partiellement rasée">Partiellement rasée</option>
-                                      <option value="Entretenu">Entretenu</option>
+                                    <select>
+                                      <option value="active">Entierement Naturel</option>
+                                      <option value="0">Entierement rasés</option>
+                                      <option value="1">Pariellement rasée</option>
+                                      <option value="2">Entretenu</option>
                                     </select>
                                   </div>
                                 </div>
                             </div>
+
                          </div>
+                     <div class="box">
                          <div class="col-xs-12"></div>
                          <div class="col-xs-3 grp">
                     <label for="tattoo">Tattoo:</label>
                 </div>
                         <div class="col-xs-9 grp"><div class="radio">
                         <label>
-                        <input type="radio" id="tatoo" name="tattoo" value="Oui">Oui</label>
+                        <input type="radio" id="tatoo" name="tatoo" value="1">Oui</label>
                     </div>
-                    <div class="radio"><label><input type="radio" id="tatoo" name="tattoo" value="Non" checked="checked">Non</label>
-                    </div>
-                    </div><div class="col-xs-3 grp"><label for="piercing">Piercings:</label>
-                </div><div class="col-xs-9 grp"><div class="radio">
-                        <label>
-                        <input type="radio" id="piercing" name="piercing" value="Oui">Oui</label>
-                    </div><div class="radio"><label>
-                    <input type="radio" id="piercing" name="piercing" value="Non" checked="checked">Non</label>
+                    <div class="radio"><label>
+                        <input type="radio" id="tatoo" name="tatoo" value="2" checked="checked">Non</label>
                     </div>
                     </div>
-
-                    
-
-                    
-                    </div>
+						 <div class="box">
+                    <div class="col-xs-3 grp">
+                        <label for="piercing">Piercings:</label>
                 </div>
-                </div> 
-            </div>
-        </div>
+                    <div class="col-xs-9 grp">
+                        <div class="radio">
+                        <label>
+                        <input type="radio" id="piercing" name="piercing" value="1">Oui</label>
+                    </div>
+						<div class="radio"><label>
+                        <input type="radio" id="piercing" name="piercing" value="2" checked="checked">Non</label>
+                    </div>
+                    </div>
+						 </div>
+                    </div>
+                    </div>
+			  </div>
+		    
         <div class="col-xs-12">
                      <div class="nextandbackBtns">
-                     <!--  <a href="javascript:void(0);" onclick="doSave();" name="submit" class="btn btn-primary save">Enregistrer</a> -->
-                     <button type="Submit" name="Save" class="btn btn-primary save">Enregistrer</button>
+                      <a href="javascript:void(0);" onclick="doSave();" class="btn btn-primary save">Enregistrer</a>
                      </div>
                     </div>
-            </div>
+            
+				
+			
+			</div>
         </form>
         </main>
         <footer>
@@ -260,6 +265,7 @@ if(!isset($_SESSION['username'])) {
                 </div>
             </div>
         </div>
+        <div class="copy"><small>© MINELUX 2020 Copyrigth All rigths reserved. V1.0 </small></div>
     </footer>
 </body>
 </html>
--- MINELUX/login2.php
+++ MINELUX/login2.php
@@ -1,9 +1,33 @@
+<?php
+require('connexionBD.php');
+//Create new Users
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+   $phone = $_POST["phone"];
+  $email = $_POST["email"];
+ 
+
+  
+  try{
+  $statement = $pdo -> prepare( "INSERT INTO `membres` (username, password, phone, email)
+        VALUES ('$username', '$password', '$phone', '$email')");
+  $statement->execute(['username'=> $username, 'password' => $password, 'phone'=> $phone, 'email'=> $email]);
+     // echo "Insert user: {username}";
+    // $id = $pdo->lastInsertId();
+     // echo "<script>location.href='clients.php?show=one&id={$id}' </script>";
+  } catch (PDOException $e) {
+      echo "<h4 style='color: red;'>".$e->getMessage(). "</h4>";
+  }
+
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
-    <tilte></tilte>
+	<tilte></tilte>
         <meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viwport" content="width=device-width, initial-scale=1">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -13,8 +37,39 @@
         <script src="js/jquery.js"></script>
 </head>
     <body id="sign_up_in">
+        <svg id="svg-source" height="0" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+        <filter id="filter-shadow" x="0" y="0" width="100%" height="100%">
+            <feGaussianBlur in="SourceAlpha" stdDeviation="3"></feGaussianBlur>
+            <feOffset dx="1" dy="1"></feOffset>
+            <feComponentTransfer>
+                <feFuncA type="linear" slope="0.3"></feFuncA>
+            </feComponentTransfer>
+            <feMerge>
+                <feMergeNode></feMergeNode>
+                <feMergeNode in="SourceGraphic"></feMergeNode>
+            </feMerge>
+        </filter>
+    </defs>
+    
+    
+    
+    
+    <g id="icon-real-photos">
+        <polygon points="16.67,4 13.85,20 16.89,20 19.1,7.48 56.52,14.07 52.18,38.69 48,37.96 48,50.14 53.05,51.03 60,11.64"></polygon>
+        <path d="M4,20v40h44V20H4z M45,48H7V23h38V48z M41,45H11l5-7l5,4l5-7l5,5l5-8L41,45z M15,28c-1.66,0-3,1.34-3,3c0,1.66,1.34,3,3,3 c1.66,0,3-1.34,3-3C18,29.34,16.66,28,15,28z"></path>
+    </g>
+    <g id="icon-discretion-guaranteed">
+        <path d="M50,28v-6c0-9.93-8.08-18-18-18c-9.93,0-18,8.07-18,18v6H8v32h48V28H50z M34,42.45V51c0,1.1-0.9,2-2,2s-2-0.9-2-2v-8.55 c-1.19-0.69-2-1.97-2-3.45c0-2.21,1.79-4,4-4s4,1.79,4,4C36,40.48,35.19,41.75,34,42.45z M18,28v-6c0-7.72,6.28-14,14-14 s14,6.28,14,14v6H18z"></path>
+    </g>
+    <g id="icon-trusted-website">
+        <path d="M49.97,19.19L49.32,14l-4.17-3.15L42,6.68l-5.19-0.65L32,4l-4.81,2.03L22,6.68l-3.15,4.17L14.68,14l-0.65,5.19L12,24 l2.03,4.81L14.68,34l4.17,3.16L22,41.32l5.19,0.65L32,44l4.81-2.03L42,41.32l3.15-4.17L49.32,34l0.65-5.19L52,24L49.97,19.19z M34.5,32.54h-4v-11.5l-3,1.5v-4l4-2h3V32.54z"></path>
+        <path d="M56,54l-7.7-1.33L45.61,60l-8.58-14.86l0.57-0.24l4.77-0.6l1.25-0.16l0.76-1.01l2.9-3.83l0.15-0.11L56,54z M26.4,44.89 l-4.77-0.6l-1.26-0.16l-0.76-1.01l-2.9-3.83l-0.15-0.11L8,54l7.7-1.33l2.7,7.33l8.58-14.86L26.4,44.89z"></path>
+    </g>
+    
+</svg>s
         <header>
-            
+        	
                 <div class="top-nav">
                 <div class="container clearfix">
                     <ul class="nav navbar-nav">
@@ -63,82 +118,24 @@
             </div>
             </div>
             </div>
-        </header>
+		</header>
         <main class="container">
             <!--<script async="" defer="" src="//www.google.com/recaptcha/api.js"></script>-->
             <!--<script funtion onsubmit(token){ document.getElementById("sign-up").submit();}></script>-->
             <div class="row">
                 <div class="col-xs-6 col-xs-offset-3">
-                    <form id="sign-up" class="clearfix" method="post" action="login2.php" id="form">
+                    <form action id="sign-up" class="clearfix" method="post" action="" id="form">
                         <div class="col-xs-10 col-xs-offset-1">
                         <h4 class="title">Inscrivez-vous gratuitement</h4><br>
-<?php
-include('connexionBD.php');
-
-if(isset($_POST['forminscription'])) {
-  $username = htmlspecialchars((trim($_POST['username'])));
-  $password = htmlspecialchars((trim($_POST['password'])));
-  $password_confirm = htmlspecialchars((trim($_POST['password_confirm'])));
-  $phone = htmlspecialchars((trim($_POST['phone'])));
-  $email = htmlspecialchars((trim($_POST['email'])));
-  $email_confirm = htmlspecialchars((trim($_POST['email_confirm'])));
-    try {
-  $sql = 'SELECT * FROM membres WHERE username LIKE ' . $pdo->quote($username);
-  $result = $pdo->query($sql);
-  $row = $result -> fetch();
-        $errorInfo = $pdo->errorInfo();
-        if (isset($errorInfo[2])) {
-            $error = $errorInfo[2];
-        }
-    } catch (Exception $e) {
-        $error = $e->getMessage();
-    }
-  if (isset($error)) {
-    echo "<p>$error</p>";
-} elseif (!$row) {
-
-    if($username && $password && $password_confirm) {
-        if(strlen($username)>4){
-            if(strlen($password)>=6){
-                if($password == $password_confirm) {
-                    if ($email == $email_confirm) {
-                       $statement = $pdo -> prepare( "INSERT INTO `membres` (username, password, phone, email)
-                             VALUES ('$username', '$password', '$phone', '$email')");
-                        $statement->execute(['username'=> $username, 'password' => $password, 'phone'=> $phone, 'email'=> $email]);
-                        echo "<p style='color: green;'>Votre compte a été créé avec succès, veuillez vous connecter</p>";
-                    } else {
-                      echo "<p style='color: red;'>Les adresses mails ne correspondent pas</p>";
-                    }
-                } else {
-                  echo "<p style='color: red;'>Les mots de passe ne correspondent pas</p>";
-                }
-            } else {
-              echo "<p style='color: red;'>Le mot de passe est trop court. 6 caracteres minimum</p>";
-            }
-        } else {
-          echo "<p style='color: red;'>Le nom d'utilisateur est trop court! minimum requis 5 caracteres</p>";
-          
-
-        }
-    } else {
-      echo "<p style='color: red;'>Veuillez remplir tous les champs!</p>";
-    }
-  } else {
-      echo "<p style='color: red;'>Ce compte existe deja</p>";
-} 
-}
-
-?>
-
                         <p class="attention">Veuillez remplir le formulaire suivant et cliquez "S'enregistrer"</p>
-                            <div class="INSCRIPTION">
+							<div class="INSCRIPTION">
                             <input type="hidden" id="user_type" name="user_type" value="2" placeholder="type de compte">
                             <input type="text" id="username" name="username" placeholder="Nom utilisateur" /><br>
                             <input type="password" id="password" name="password" placeholder="Mot de passe"><br>
                             <input type="password" id="password_confirm" name="password_confirm" placeholder="Confirmer le mot de passe"><br>
                             <input type="tel" id="phone" name="phone" min="9" max="12"  placeholder="Numero Telephone"><br>
                             <input type="text" id="email" name="email" placeholder="Courrier*"/><br>
-                            <input type="text" id="email" name="email_confirm"  placeholder="Confirmer*">
+                            <input type="text" id="email" name="email2"  placeholder="Confirmer*">
                         </div><br>
                         <div class="custom-checkbox">
                             <input type="checkbox" id="checkterm" name="terms" value="1">
@@ -189,6 +186,7 @@ if(isset($_POST['forminscription'])) {
                 </div>
             </div>
         </div>
+        <div class="copy"><small>© MINELUX 2020 Copyrigth All rigths reserved. V1.0 </small></div>
     </footer>
 </body>
 </html>
